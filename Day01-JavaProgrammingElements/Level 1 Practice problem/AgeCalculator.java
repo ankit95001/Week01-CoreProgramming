476ba249c0c1890b4e7f4c08c698bef1702719dc
Ankit_kumar <ankit95001kumar@gmail.com>
2025-01-09 16:51:31 +0530

    AgeCalculator done

---
+++ Day01-JavaProgrammingElements/Level 1 Practice problem/AgeCalculator.java
@@ -0,0 +1,17 @@
+// Create AgeCalculator Class to compute the age of Harry
+class AgeCalculator{
+	public static void main(String[] args){
+		
+		//Storing birth year of Harry in year variable
+		int year = 2000;
+		
+		//calculating the age
+		int currentYear = 2024;
+		
+		int age = currentYear-year;
+		
+		//Display the age of Harry if the birth year is 2000
+		System.out.println("Harry's age in 2024 is " + age);
+	}
+}
+	
