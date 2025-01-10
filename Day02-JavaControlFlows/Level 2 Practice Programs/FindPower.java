463cd6838e24e4b2a7c5ed90ad7753aa7432d4c8
Ankit_kumar <ankit95001kumar@gmail.com>
2025-01-10 14:44:27 +0530

    Find power done

---
+++ Day02-JavaControlFlows/Level 2 Practice Programs/FindPower.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+//Creating the class FindPower for finding the power of number
+public class FindPower{
+    public static void main(String[] args) {
+		//creating the scanner class object for input
+        Scanner input = new Scanner(System.in);
+
+        // Taking input from user
+        System.out.print("Enter the base number- ");
+        int number = input.nextInt();
+
+        System.out.print("Enter the power- ");
+        int power = input.nextInt();
+
+        // Initializing result variable
+        int result = 1;
+
+        // Calculating the power using a loop
+        for (int i = 1; i <= power; i++) {
+            result *= number;
+        }
+
+        // Displaying the result
+        System.out.println(number + " raised to the power " + power + " is: " + result);
+		//close the input object
+        input.close();
+    }
+}
