00e1201d208aaab60266cfda6093b66e0e347b8b
Ankit_kumar <ankit95001kumar@gmail.com>
2025-01-10 14:44:48 +0530

    FizzBuzz done using for loop

---
+++ Day02-JavaControlFlows/Level 2 Practice Programs/FizzBuzzForLoop.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+//Creating class FizzBuzzForLoop for calculation
+class FizzBuzzForLoop{
+    public static void main(String[] args) {
+		//Creating scanner object for input
+        Scanner input = new Scanner(System.in);
+
+        // Taking as input
+        System.out.print("Enter a number to generate FizzBuzz- ");
+        int number = input.nextInt();
+		
+		//Check if number is less than 0
+		if(number<0){
+			return;
+		}
+		
+        // Generating FizzBuzz pattern
+        for(int sequence=0;sequence<=number;sequence++){
+			//Check for the condition if number is divisible by 3 and 5
+			if(sequence%3==0 && sequence%5==0){
+				System.out.println("FizzBuzz");
+			}
+			//Check for the condition if number is divisible by 3 
+			else if(sequence%3==0){
+				System.out.println("Fizz");
+			}
+			//Check for the condition if number is divisible by 5
+			else if(sequence%5==0){
+				System.out.println("Buzz");
+			}
+			else{
+				System.out.println(sequence);
+			}
+		}	
+		//closing the sacnner object
+        input.close();
+    }
+}
