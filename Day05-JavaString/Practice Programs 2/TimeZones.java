7c33a325a9fb647957723cd33f8b1f02793b905b
Ankit_kumar <ankit95001kumar@gmail.com>
2025-01-13 14:55:59 +0530

    Time zone done

---
+++ Day05-JavaString/Practice Programs 2/TimeZones.java
@@ -0,0 +1,19 @@
+import java.time.ZonedDateTime;
+import java.time.ZoneId;
+
+public class TimeZones {
+    public static void main(String[] args) {
+	
+        // Display current time in GMT
+        ZonedDateTime gmtTime = ZonedDateTime.now(ZoneId.of("GMT"));
+        System.out.println("Current time in GMT: " + gmtTime);
+
+        // Display current time in IST
+        ZonedDateTime istTime = ZonedDateTime.now(ZoneId.of("Asia/Kolkata"));
+        System.out.println("Current time in IST: " + istTime);
+
+        // Display current time in PST
+        ZonedDateTime pstTime = ZonedDateTime.now(ZoneId.of("America/Los_Angeles"));
+        System.out.println("Current time in PST: " + pstTime);
+    }
+}
