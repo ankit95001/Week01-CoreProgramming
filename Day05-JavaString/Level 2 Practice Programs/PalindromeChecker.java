3709c2d2735e825078f88edaed60081164e1e39e
Ankit_kumar <ankit95001kumar@gmail.com>
2025-01-14 16:01:15 +0530

    Palindrome Checker done

---
+++ Day05-JavaString/Level 2 Practice Programs/PalindromeChecker.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+//creating class PalindromeChecker for checking
+class PalindromeChecker{
+	public static boolean check(String string){
+		boolean flag = true;
+		for(int i=0;i<string.length()/2;i++){
+			if(string.charAt(i) != string.charAt(string.length()-i-1)){
+				flag=false;
+				break;
+			}
+		}
+		return flag;
+	}
+	public static void main(String[] args){
+		
+		String string = "adba";
+		
+		//calling method to get result
+		boolean result = check(string);
+		
+		//printing the result
+		System.out.println("Is Palindrome "+string+" is : "+result);
+		
+	}
+}
+
